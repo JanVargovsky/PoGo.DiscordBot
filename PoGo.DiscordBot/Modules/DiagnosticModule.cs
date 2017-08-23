8b1f9810e7b17c5e2260579f4a127b90f41d280e
Jan Vargovský <jan.vargovsky@gmail.com>
2017-08-23 22:35:05 +0200

    Diagnostic module, ps command (basic info about process: time, memory, cpu)

---
+++ PoGo.DiscordBot/Modules/DiagnosticModule.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PoGo.DiscordBot.Modules
+{
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public class DiagnosticModule : ModuleBase
+    {
+        [Command("ps")]
+        public async Task ProcessInfo()
+        {
+            var proc = Process.GetCurrentProcess();
+            double mem = proc.WorkingSet64;
+            var cpu = proc.TotalProcessorTime;
+
+            var suffixes = new[] { "", "K", "M", "G", "T" };
+            int memoryIndex = 0;
+            while (mem >= 1024)
+            {
+                mem /= 1024;
+                memoryIndex++;
+            }
+
+            var totalTime = DateTime.Now - proc.StartTime;
+
+            EmbedBuilder embedBuilder = new EmbedBuilder()
+                .AddField("Time running", $"{totalTime}")
+                .AddField("Memory", $"{mem:n3} {suffixes[memoryIndex]}B")
+                .AddField("CPU", $"{cpu.TotalSeconds:n3} sec")
+                .AddField("Average CPU", $"{(cpu.TotalMilliseconds / totalTime.TotalMilliseconds):n3} %")
+                ;
+
+            await ReplyAsync(string.Empty, embed: embedBuilder.Build());
+        }
+    }
+}
