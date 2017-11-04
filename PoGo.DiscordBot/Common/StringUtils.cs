a1676344400485e3347465d5d5f6d6bf5cdb27eb
Jan Vargovský <jan.vargovsky@gmail.com>
2017-11-04 17:12:58 +0100

    StringUtils

---
+++ PoGo.DiscordBot/Common/StringUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace PoGo.DiscordBot.Common
+{
+    public static class StringUtils
+    {
+        public static string ToLowerWithoutDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(text.Length);
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(char.ToLower(c));
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
--- PoGo.DiscordBot/Services/GymLocationService.cs
+++ PoGo.DiscordBot/Services/GymLocationService.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.Extensions.Options;
+using PoGo.DiscordBot.Common;
 using PoGo.DiscordBot.Configuration.Options;
 using PoGo.DiscordBot.Dto;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
 
 namespace PoGo.DiscordBot.Services
 {
@@ -16,37 +15,20 @@ namespace PoGo.DiscordBot.Services
         {
             gymsInfos = options.Value.Guilds
                 .Where(t => t.Gyms?.Any() == true)
-                .ToDictionary(t => t.Id, t => t.Gyms.Select(g => (ToLowerWithoutDiacritics(g.Name), new GymInfoDto
+                .ToDictionary(t => t.Id, t => t.Gyms.Select(g => (StringUtils.ToLowerWithoutDiacritics(g.Name), new GymInfoDto
                 {
                     Name = g.Name,
                     Latitude = g.Latitude,
                     Longitude = g.Longitude
                 })).ToArray());
-        }
-
-        string ToLowerWithoutDiacritics(string text)
-        {
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder(text.Length);
-
-            foreach (var c in normalizedString)
-            {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(char.ToLower(c));
-                }
-            }
-
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
+        }        
 
         public IEnumerable<GymInfoDto> Search(ulong guildId, string name)
         {
             if (!gymsInfos.TryGetValue(guildId, out var gyms))
                 return null;
 
-            var normalizedName = ToLowerWithoutDiacritics(name);
+            var normalizedName = StringUtils.ToLowerWithoutDiacritics(name);
             return gyms
                 .Where(t => t.NormalizedName.Contains(normalizedName))
                 .Select(t => t.GymInfo);
